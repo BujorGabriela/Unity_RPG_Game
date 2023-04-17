6ba0a45fcd13ec5e582feef27d469108ce2244be
BujorGabriela <gbujor4@gmail.com>
2023-04-17 13:31:27 +0300

    Fix movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D playerRigidBody; 
+    [SerializeField] Rigidbody2D playerRigidBody;
+    [SerializeField] Animator playerAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +18,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
-        float verticallMovement = Input.GetAxisRaw("Vertical");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
 
-        playerRigidBody.velocity = new Vector2(horizontalMovement, verticallMovement);
+        playerRigidBody.velocity = new Vector2(horizontalMovement, verticalMovement);
+
+        playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
+
+        if(horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalMovement);
+            playerAnimator.SetFloat("lastY", verticalMovement);
+        }
     }
 }
