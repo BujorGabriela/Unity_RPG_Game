8c0c0daba2dbcd1ab9dcf0ea859bb6f28f6675d7
BujorGabriela <gbujor4@gmail.com>
2023-04-17 18:27:42 +0300

    Added switch scenes

--- Assets/Scripts/AreaExit.cs
+++ Assets/Scripts/AreaExit.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AreaExit : MonoBehaviour
 {
+    [SerializeField] string sceneToLoad;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +24,7 @@ public class AreaExit : MonoBehaviour
     {
         if(collision.CompareTag("Player"))
         {
-            UnityEngine.Debug.Log("This is a player that entered");
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
