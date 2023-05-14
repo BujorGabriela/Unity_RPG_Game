9ae4d5044d65378e8a1572db976e7edc8989be55
AminaUVT <amina.ali03@e-uvt.ro>
2023-05-14 20:50:06 +0300

    added xp stat

--- Assets/Scripts/Characters/PlayerStats.cs
+++ Assets/Scripts/Characters/PlayerStats.cs
@@ -7,8 +7,11 @@ public class PlayerStats : MonoBehaviour
 
     [SerializeField] string playerName;
 
+    [SerializeField] int maxLevel = 50;
     [SerializeField] int playerLevel = 1;
     [SerializeField] int currentXP;
+    [SerializeField] int[] xpForNextLevel;
+    [SerializeField] int baseLevelXP = 100;
 
     [SerializeField] int maxHP = 100;
     [SerializeField] int currentHP;
@@ -22,12 +25,31 @@ public class PlayerStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        xpForNextLevel = new int[maxLevel];
+        xpForNextLevel[1] = baseLevelXP;
+
+        for(int i = 2; i < xpForNextLevel.Length; i++)
+        {
+            xpForNextLevel[i] = (int)(0.02f * i * i * i + 3.06f * i * i + 105.6f * i); //from the dark souls remastered wiki
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            AddXP(100);
+        }
+    }
+
+    public void AddXP(int amountOfXp)
+    {
+        currentXP += amountOfXp; // currentXP = currentXP + amountOfXp
+        if(currentXP > xpForNextLevel[playerLevel])
+        {
+            currentXP -= xpForNextLevel[playerLevel];
+            playerLevel++;
+        }
     }
 }
